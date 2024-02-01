586bcbe064158691da542470bb3cedcd235773e3
Sneha Panditpautra <snehapanditpautra14@gmail.com>
2024-02-01 22:35:04 +0530

    Advent calendar List

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days=[...Array(24)].map((val,index)=>index+1);
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({item})=> <DayListItem day={item}/>}
+
+      />
+
+
+
+
+
+     
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    aspectRatio: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content:{
+    gap:10,
+    padding:10,
+  },
+  column:{
+    gap:10
+
+  },
+  box: {
+    backgroundColor:'#F9EDE3',
+    // width:300,
+    // height:300,
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor:'#9b4521',
+    borderRadius:20,
+
+  },
+  text:{
+    color:'#9B4521',
+    fontSize:70,
   },
 });
---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,33 @@
+import {Text, View,StyleSheet} from 'react-native';
+
+type DayListItem={
+    day:number;
+}
+
+export default function DayListItem({day}:DayListItem) {
+    return (
+        <View style={styles.box}>
+        <Text style={styles.text}>{day}</Text>
+       </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    box: {
+      backgroundColor:'#F9EDE3',
+      // width:300,
+      // height:300,
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor:'#9b4521',
+      borderRadius:20,
+  
+    },
+    text:{
+      color:'#9B4521',
+      fontSize:70,
+    },
+  });
